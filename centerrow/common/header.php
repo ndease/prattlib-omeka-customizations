dfc3b405ef4cc78b76ba96572214ef31ab1ecb5b
Nick Dease <ndease@pratt.edu>
2021-07-06 12:42:48 -0400

    replace bootstrap

--- centerrow/common/header.php
+++ centerrow/common/header.php
@@ -36,7 +36,7 @@
 <!-- Pratt Style -->
 <link rel="stylesheet" href="/themes/centerrow/css/pratt-style.css?ver=010" type="text/css" />
 <!-- BootStrap -->
-  <link rel="stylesheet" href="https://library.pratt.edu/?css=site2019/.bootstrap_min.v.1556638942" type="text/css" />
+  <link rel="stylesheet" href="https://library.pratt.edu/css/bootstrap.min.css" type="text/css" />
    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
